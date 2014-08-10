9fef0f7fbd5e1c6ee1371556cf961f27f25d7f72
hzqlmm <hzqlmm@gmail.com>
2014-08-10 17:00:15 +0800

    no desc

---
+++ marketMaker/keep-currency-balance-solution-1.js
@@ -0,0 +1,177 @@
+var math = require('mathjs');
+var _ = require('underscore');
+var events = require('events');
+
+var config = require('./config.js');
+var ripple = require('../src/js/ripple');
+var crypto = require('./crypto-util.js');
+var jsbn = require('../src/js/jsbn/jsbn.js');
+var mongodbManager = require('./mongodb-manager.js');
+var Logger = require('./the-future-logger.js').TFLogger;
+Logger.getNewLog("keep-currency-balance-solution-1");
+
+var emitter = new events.EventEmitter();
+emitter.once('decrypt', decrypt);
+emitter.once('remoteConnect', remoteConnect);
+
+var remote_options = remote_options = {
+    // see the API Reference for available options
+    // trace: true,
+    trusted: true,
+    local_signing: true,
+    local_fee: true,
+    fee_cushion: 1.5,
+    max_fee: 100,
+    servers: [{
+        host: 's-east.ripple.com',
+        port: 443,
+        secure: true
+    }, {
+        host: 's-west.ripple.com',
+        port: 443,
+        secure: true
+    }, {
+        host: 's1.ripple.com',
+        port: 443,
+        secure: true
+    }]
+};
+
+var remote = new ripple.Remote(remote_options);
+
+var account;
+var secret;
+mongodbManager.getAccount(config.mother, function(result) {
+    account = result.account;
+    secret = result.secret;
+    emitter.emit('decrypt', secret);
+});
+
+var offers;
+var orders = [];
+
+function decrypt(encrypted) {
+    crypto.decrypt(encrypted, function(result) {
+        secret = result;
+        emitter.emit('remoteConnect');
+    });
+}
+
+function remoteConnect() {
+    remote.connect(function() {
+        remote.requestAccountOffers(account, function() {
+            offers = arguments[1].offers;
+            remote.requestAccountLines(account, function(err, result) {
+                if (err) console.log(err);
+                averageBalance(result.lines);
+            });
+        });
+    });
+}
+
+function averageBalance(lines) {
+    lines = _.filter(lines, function(line) {
+        return line.limit != 0;
+    });
+
+    lines = _.groupBy(lines, function(line) {
+        return line.currency;
+    })
+
+    var newLines = {};
+    var highLimits = {};
+    var lowLimits = {};
+    var currencies = _.keys(lines);
+    _.each(currencies, function(currency) {
+        if (lines[currency].length > 1) {
+            lines[currency] = _.sortBy(lines[currency], function(line) {
+                return line.limit;
+            });
+
+            var limit = _.last(lines[currency]).limit;
+
+            highLimits[currency] = _.filter(lines[currency], function(line) {
+                return line.limit == limit;
+            });
+
+            lowLimits[currency] = _.filter(lines[currency], function(line) {
+                return line.limit != limit && line.balance / line.limit >= 0.95;
+            });
+        }
+    });
+
+    console.log("highLimits:");
+    console.log(highLimits);
+    console.log("lowLimits:");
+    console.log(lowLimits);
+
+    currencies = _.keys(lowLimits);
+    _.each(currencies, function(currency) {
+        if (lowLimits[currency].length > 0) {
+            _.each(lowLimits[currency], function(low) {
+                var list = highLimits[currency];
+                if (list) {
+                    _.each(list, function(high) {
+                        var taker_pays = {
+                            'currency': currency,
+                            'issuer': high.account,
+                            'value': low.balance
+                        };
+
+                        var taker_gets = {
+                            'currency': currency,
+                            'issuer': low.account,
+                            'value': low.balance,
+                        };
+                        if (!ifOfferExist(offers, taker_pays, taker_gets)) {
+                            createOffer(taker_pays, taker_gets);
+                        }
+                    });
+                }
+            });
+        }
+    });
+}
+
+
+function createOffer(taker_pays, taker_gets) {
+    var tx = remote.transaction();
+    if (secret) {
+        tx.secret(secret);
+    } else {
+        return;
+    }
+
+    Logger.log(true, "we are create offer here", "taker_pays", taker_pays, "taker_gets", taker_gets);
+
+    tx.offerCreate(account, taker_pays, taker_gets);
+    tx.on("success", function(res) {
+        console.log("create offer success");
+    });
+    tx.on("error", function(res) {
+        console.log(res);
+        throw new Error('something wrong!!!!');
+    });
+
+    tx.submit();
+}
+
+function ifOfferExist(offers, pays, gets) {
+    var self = this;
+
+    var result = _.filter(offers, function(offer) {
+        return offer.taker_pays.currency == pays.currency && offer.taker_pays.issuer == pays.issuer && offer.taker_gets.currency == gets.currency && offer.taker_gets.issuer == gets.issuer;
+    });
+
+    if (result.length > 0) {
+        return true;
+    }
+
+    return false;
+}
+
+setTimeout(throwDisconnectError, 1000 * 60 * 3);
+
+function throwDisconnectError() {
+    throw new Error('we are disconnect with ripple network!!!');
+}
