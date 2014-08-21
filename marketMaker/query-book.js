6bc077a9241fceebfa7f9407ff9587f9db5f95a1
hzqlmm <hzqlmm@gmail.com>
2014-08-21 12:29:37 +0800

    Update query-book.js

--- marketMaker/query-book.js
+++ marketMaker/query-book.js
@@ -1,6 +1,6 @@
 var filterOffers = require('./offer-filter.js').filterOffers;
 
-function queryBook(remote, currency1, issuer1, currency2, issuer2, account) {
+function queryBook(remote, currency1, issuer1, currency2, issuer2, account, logger) {
     var bookInfo = {};
 
     var asks = remote.book(currency1, issuer1, currency2, issuer2);
@@ -9,10 +9,10 @@ function queryBook(remote, currency1, issuer1, currency2, issuer2, account) {
             var newOffers = filterOffers(offers, currency1, currency2, account, "asks");
 
             bookInfo.price = newOffers[0].TakerPays.ratio_human(newOffers[0].TakerGets).to_human().replace(',', '');
-            bookInfo.takerPays = newOffers[0].TakerPays.to_json();
-            bookInfo.TakerGets = newOffers[0].TakerGets.to_json();
+            bookInfo.taker_pays = newOffers[0].TakerPays.to_json();
+            bookInfo.taker_gets = newOffers[0].TakerGets.to_json();
 
-            console.log(bookInfo);
+            logger.log(true, bookInfo);
         }
     });
 }
